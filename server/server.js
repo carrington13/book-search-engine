00254d2ce7857c72fa333a6eeda5ed9d906ec4ac
Mediogre <caseyarrington13@gmail.com>
2021-07-10 10:15:51 -0500

    modified server to add ApolloServer, add typeDefs and resolvers file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+// import ApolloServer
+const {ApolloServer} = 'apollo-server-express'
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
+// import schemas and auth
+const { typeDefs, resolvers} = require('./schemas');
+const { authMiddleware } = require('./auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// set ApolloServer and pass in schemas and authMiddleWare
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
 
+server.applyMiddleware({ app });
+
+// TODO? turn to false later
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +28,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+//app.use(routes);
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`) 
+  });
 });
+
